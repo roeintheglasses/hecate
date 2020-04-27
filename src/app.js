7f93ba2a500831c88535fdae502ffbd98cec7b12
Hrishi Jangir <hrkjangir@gmail.com>
2020-04-28 01:45:35 +0530

    Restructured directory and changed Stylesheet

---
+++ src/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("../models/shortUrl");
+const path = require('path')
+
+const app = express();
+const port = process.env.PORT || 3000;
+const viewsPath = path.join(__dirname, '../views')
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+
+mongoose.connect("mongodb://localhost/hecate", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+app.set("view engine", "ejs");
+app.set('views', viewsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.get("/", async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render("index", {
+        shortUrls
+    });
+});
+
+app.post("/short", async (req, res) => {
+    await ShortUrl.create({
+        full: req.body.fullUrl,
+    });
+    res.redirect("/");
+});
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({
+        short: req.params.shortUrl
+    });
+    if (shortUrl == null)
+        return res.send.status(404);
+
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
+})
+
+app.listen(port, () => {
+    console.log("App running on port " + port);
+});
